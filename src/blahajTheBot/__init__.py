23b428ade0083e2bf3031a8add5446ad74c96927
Michal Rajzer <michal.rajzer03@gmail.com>
2023-03-16 01:30:28 +0100

    Added new cogs + refactoring

--- src/blahajTheBot/__init__.py
+++ src/blahajTheBot/__init__.py
@@ -5,8 +5,14 @@ from nextcord.ext import commands
 import logging
 
 from basic import Basic
+from mod import Mod
+from setup import Setup
 
+# Load .env
+load_dotenv()
 
+# Set up logging
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('nextcord')
 logger.setLevel(logging.INFO)
 handler = logging.FileHandler(
@@ -15,16 +21,21 @@ handler.setFormatter(logging.Formatter(
     '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-logging.basicConfig(level=logging.INFO)
 
-load_dotenv()
-
+# Set intents
 intents = nextcord.Intents.default()
 intents.message_content = True  # Needed for automod
+intents.members = True
+
+
+# Create bot
 bot = commands.Bot(intents=intents,)
 
-
+# Load cogs
 bot.add_cog(Basic(bot))
+bot.add_cog(Mod(bot))
+bot.add_cog(Setup(bot))
 
-
-bot.run(os.getenv("BOT_TOKEN"))
+# Run bot
+if __name__ == '__main__':
+    bot.run(os.getenv("BOT_TOKEN"))
