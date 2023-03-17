051718957a41ee62da5998cdb56ee80748b48869
Michal Rajzer <michal.rajzer03@gmail.com>
2023-03-17 07:55:22 +0100

    Added delete + more logging

--- src/blahajTheBot/db/configs.py
+++ src/blahajTheBot/db/configs.py
@@ -1,13 +1,17 @@
 import boto3
-from dotenv import load_dotenv
+import os
 import logging
+from dotenv import load_dotenv
 
 
 class Configs_DB:
     def __init__(self) -> None:
+        logging.debug("Initializing Configs_DB...")
         load_dotenv()
-        self.dynamodb = boto3.resource('dynamodb', region_name='eu-north-1')
+        self.dynamodb = boto3.resource('dynamodb', region_name='eu-north-1',
+                                       aws_access_key_id=os.getenv("AWS_ID"), aws_secret_access_key=os.getenv("AWS_SECRET"))
         self.table = self.dynamodb.Table('blahajthebot_configs')
+        logging.info("Initialized Configs_DB!")
 
     def get_config(self, guild_id: int, config_name: str) -> str | None:
         """Gets a config from the DB. Returns None if not found otherwise returns the config string"""
@@ -37,4 +41,16 @@ class Configs_DB:
             }
         )
         # maybe will work?
+        return response['ResponseMetadata']['HTTPStatusCode'] == 200
+
+    def delete_config(self, guild_id: int, config_name: str) -> bool:
+        """Deletes a config from the DB. Returns True if successful, False if not"""
+        logging.INFO(
+            f"Deleting config ({config_name}) for {guild_id} from DB...")
+        response = self.table.delete_item(
+            Key={
+                'guild_id': guild_id,
+                'config_name': config_name
+            }
+        )
         return response['ResponseMetadata']['HTTPStatusCode'] == 200
