8da12c4082beb293d629ae06a42152dc2632a743
Michal Rajzer <michal.rajzer03@gmail.com>
2023-03-17 07:59:41 +0100

    Added an info command

--- src/blahajTheBot/basic/__init__.py
+++ src/blahajTheBot/basic/__init__.py
@@ -15,6 +15,14 @@ class Basic(commands.Cog):
             title="**Pong!**", description="**" + ping1 + "**", color=0x3477eb)
         await interaction.response.send_message(embed=embed)
 
+    @nextcord.slash_command(description="Use this to get info about the bot!")
+    async def info(self, interaction: nextcord.Interaction) -> None:
+        embed = nextcord.Embed(
+            title="**Blahaj the Bot**", description="**Blahaj the Bot is a bot for general purpose bot behaviours made by PolishFuze#1337!**", color=0x3477eb)
+        embed.add_field(name="**__Links__**",
+                        value="**[Invite](https://discord.com/api/oauth2/authorize?client_id=975443159112884286&permissions=8&scope=bot) | [Support Server](https://discord.gg/rkYhSpKjZY) | [Developer](https://rajzer.dev)**", inline=False)
+        await interaction.response.send_message(embed=embed)
+
     @commands.Cog.listener()
     async def on_ready(self):
         logging.info("Bot is ready!")
